1a3f607ad4a249d02ba6a1c721b5c6fcbb45d38e
Junjun Guo <guojunjun@gmail.com>
2016-12-23 18:58:31 +0100

    spring boot, spring security, hibernate, H2, JWT

---
+++ spring-boot-jwt-h2/README.md
@@ -0,0 +1,6 @@
+#### Spring Boot 
+#### Spring Security
+#### Hibernate
+#### JSON WEB TOKENS (JWT)
+#### H2 database
+ 
---
+++ spring-boot-jwt-h2/src/main/java/com/junjunguo/jwt/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.junjunguo.jwt.repository;
+
+import com.junjunguo.jwt.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
